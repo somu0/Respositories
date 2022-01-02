7afb80a1eac33c7b65ffb0ea305624bc32436cda
choppasumanth <94858638+choppasumanth@users.noreply.github.com>
2022-01-02 11:04:18 +0530

    Create ass1.js

---
+++ 20dec/ass1.js
@@ -0,0 +1,14 @@
+number=prompt('Enter your Number: ')
+
+if(number>10)
+{
+console.log("number is greater than 10");
+}
+else if(number<10)
+{
+console.log("number is lesser than 10");
+}
+else
+{
+console.log("number is equal to 10");
+}
