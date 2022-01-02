d4966c8abb97afd8e1fd33522e51df97f25c913a
choppasumanth <94858638+choppasumanth@users.noreply.github.com>
2022-01-02 11:48:40 +0530

    Create ass2.js

---
+++ 21dec/ass2.js
@@ -0,0 +1,19 @@
+var  char=prompt("Enter the character");
+if (char=="a" || char == "A"){
+    alert("It is vowel");
+}
+else if (char=="e" || char == "E"){
+    alert("It is vowel");
+}
+else if (char=="i" || char == "I"){
+    alert("It is vowel");
+}
+else if (char=="o" || char == "O"){
+   alert("It is vowel");
+}
+else if (char=="u" || char == "U"){
+    alert("It is vowel");
+}
+else{
+    alert("It is consonant");
+}
