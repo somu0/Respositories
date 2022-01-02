8c381ab3790aad3547144e805a1e697414bb2392
choppasumanth <94858638+choppasumanth@users.noreply.github.com>
2022-01-02 11:15:12 +0530

    Create ass1.js

---
+++ 21dec/ass1.js
@@ -0,0 +1,12 @@
+a=prompt("Enter your first number")
+b=prompt("Enter your second number ")
+c=prompt("Enter your third numbner")
+if (a<b && a<c){
+    console.log("a is minimum")
+}
+else if(b<a && b<c){
+    console.log("b is minimum")
+}
+else{
+    console.log("c is minimum")
+}
