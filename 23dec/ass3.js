245ba3d398c8c60e6b52e877951a11af86057a1f
choppasumanth <94858638+choppasumanth@users.noreply.github.com>
2022-01-02 12:54:23 +0530

    Create ass3.js

---
+++ 23dec/ass3.js
@@ -0,0 +1,28 @@
+var g_class={
+    "class"="programming class",
+    "instructor":{
+    "name"="Aayush sinha",
+    "subject"="MERN"
+},
+"students"=[{
+    "Name"="choppa",
+    "marks"=56,
+},
+{
+    "Name"="lokesh",
+    "marks"=66,
+},
+{
+    "name"="charan",
+    "marks"=78,
+},
+{
+    "Name"="vinoth",
+    "marks"=90,
+}
+],
+};
+console.log(g_class.instructor.name.);
+console.log(g_class["instructor"]["name"]);
+console.log(g_class.students["4"].name);
+
