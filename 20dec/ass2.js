a706ebd170c0f2bf2db297e7155967e2c5e86df2
choppasumanth <94858638+choppasumanth@users.noreply.github.com>
2022-01-02 11:04:52 +0530

    Create ass2.js

---
+++ 20dec/ass2.js
@@ -0,0 +1,12 @@
+a=prompt("enter your first number")
+b=prompt("enter your second number")
+c=prompt("enter your third number")
+if (a>b && a>c){
+    console.log("a is Maximum")
+}
+else if(b>a && b>c){
+    console.log("b is maximum")
+}
+else{
+    console.log("c is maximum")
+}
