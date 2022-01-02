4f3ba972bbffce7bca8b7ce57b49a50c41d9cfdc
choppasumanth <94858638+choppasumanth@users.noreply.github.com>
2022-01-02 12:10:55 +0530

    Update ass.js

--- 22dec/ass.js
+++ 22dec/ass.js
@@ -9,11 +9,11 @@ var c=prompt("Enter 3rd Number:");
 c=parseInt(c);
 arr.push(c);
 if((arr[0]>=arr[1]) && (arr[0]>=arr[2])){
-    alert(arr[0]," is Maximum");
+    alert(arr[0],);
 }
 else if((arr[1]>=arr[0] && (arr[1]>=arr[2]))){
-    alert(arr[1],"is Maximum");
+    alert(arr[1]);
 }
 else{
-    alert(arr[2],"is Maximum");
+    alert(arr[2]);
 }
