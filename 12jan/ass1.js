8028272490c7689916bc72b1dc4d534a6d59cb86
choppasumanth <94858638+choppasumanth@users.noreply.github.com>
2022-01-13 13:30:48 +0530

    Update ass1.js

--- 12jan/ass1.js
+++ 12jan/ass1.js
@@ -1,8 +1,8 @@
 var likecount=document.querySelector("#likecount");
 var dislikecount=document.querySelector("#dislikecount");
-function count(){
+function count1(){
     likecount.value=parseInt(likecount.value) +1;
 }
-function count1(){
+function count2(){
     dislikecount.value=parseInt(dislikecount.value) +1;
 }
