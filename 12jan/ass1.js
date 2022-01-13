1a3625836401af1764b1b86b4b72e96f15decc2c
choppasumanth <94858638+choppasumanth@users.noreply.github.com>
2022-01-13 13:25:26 +0530

    Add files via upload

---
+++ 12jan/ass1.js
@@ -0,0 +1,8 @@
+var likecount=document.querySelector("#likecount");
+var dislikecount=document.querySelector("#dislikecount");
+function count(){
+    likecount.value=parseInt(likecount.value) +1;
+}
+function count1(){
+    dislikecount.value=parseInt(dislikecount.value) +1;
+}
